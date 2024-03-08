1f1c1e75f36f62f6084ce751197151972c93b9bd
VictorFCosta2007 <victorferreiracosta@gmail.com>
2024-03-07 22:51:11 -0300

    Lista 3

---
+++ Listas/Lista 3/Exercício 02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string [] args)
+    {
+        System.Console.WriteLine("Quantidade de itens no carrinho");        
+        int qtdeItensComprados = int.Parse(Console.ReadLine());       
+        
+        double precoTotal = 0;
+
+        int contador = 1;
+        
+        const double FRETE_GRATIS = 150;
+        
+        while(contador <= qtdeItensComprados)
+        {
+            System.Console.WriteLine($"Preco do {contador}º produto");
+            double preco = double.Parse(Console.ReadLine());
+
+            precoTotal = precoTotal + preco;
+            contador++;
+        }   
+        if(precoTotal >= FRETE_GRATIS)
+            {
+                System.Console.WriteLine($"Parabens! Voce ganhou frete gratis em sua compra de valor {precoTotal}");
+            }
+            else
+            {
+                System.Console.WriteLine($"O valor da sua compra ficou de: {precoTotal}");
+            }
+    
+    
+    }
+}
---
+++ Listas/Lista 3/Exercício 03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string USUARIO_CORRETO = "admin";
+        const string SENHA_CORRETA = "123senha";
+        const int NUMERO_TENTATIVAS = 3;
+        int contador = 0;
+
+        do
+        {
+            System.Console.WriteLine("Digite o nome de usuario:");
+            string usuarioInserido = Console.ReadLine();
+
+            System.Console.WriteLine("Digite sua senha:");
+            string senhaInserida = Console.ReadLine();
+
+            if(usuarioInserido == USUARIO_CORRETO && senhaInserida == SENHA_CORRETA)
+            {
+                System.Console.WriteLine("Login realizado com sucesso!!");
+                break;
+            }
+            else
+            {
+                System.Console.WriteLine("Nome de usuario ou senha incorretos. Tente novamente");
+
+                contador++;
+                if(contador >= NUMERO_TENTATIVAS)
+                {
+                    System.Console.WriteLine("Sua conta foi bloqueada");
+                    break;
+                }
+            }
+        
+        }
+        while(contador <= NUMERO_TENTATIVAS);        
+    }
+}
