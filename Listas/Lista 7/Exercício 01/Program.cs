85e6079ef3e15206161549276cb4ecd5315cc491
VictorFCosta2007 <victorferreiracosta@gmail.com>
2024-04-05 00:07:51 -0300

    Lista 7

---
+++ Listas/Lista 7/Exercício 01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int numero = Convert.ToInt32(Console.ReadLine()); 
+
+        System.Console.WriteLine(VerificacaoNumero(numero)); 
+    }
+    static bool VerificacaoNumero(int numero)
+    {
+        return numero % 2 == 0;
+    }
+}
---
+++ Listas/Lista 7/Exercício 05/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_MATRIZ = 4;
+        int[,] matriz = new int [TAMANHO_MATRIZ,TAMANHO_MATRIZ];
+    
+        PreencherMatriz(matriz);
+        ImprimirTabuleiro(matriz);
+
+        VerificarBalistaExistente(matriz);
+    }
+
+    static void PreencherMatriz(int[,] matriz)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                matriz[i,j] = Convert.ToInt32(Console.ReadLine()); 
+            }
+        }
+    }
+
+    static void ImprimirTabuleiro(int[,] matriz)
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                System.Console.Write($"|{matriz[i,j]}|");
+            }
+            System.Console.WriteLine();
+        }
+    }
+    
+    
+    static void VerificarBalistaExistente(int[,] matriz)
+    {
+        bool temBalista = false;
+        
+        for(int i = 0; i < 4; i++)
+        {
+            for(int j = 0; j < 4; j++)
+            {
+                if(matriz[i,j] == 0)
+                {
+                    temBalista = true;
+                } 
+            }
+        }
+        if(temBalista)
+        {
+            System.Console.WriteLine("Fique atento! existem balistas no tabuleiro");
+        } 
+        else
+        {
+            System.Console.WriteLine("nao ha balistas no tabuleiro");
+        }
+    }
+}
